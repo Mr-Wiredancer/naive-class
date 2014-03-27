b1ed600a860e2cf3157a5d672eaf560bdfaa69f8
Mr. Wiredancer <jiahao@dxhackers.com>
2014-03-27 10:14:54 +0800

    implement instance methods

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 module.exports = function( data ){
     var c = function(){
-        if (data.initialize){
+        if ( data && data.hasOwnProperty('initialize') && (typeof data.initialize === 'function') ){
             data.initialize.apply(this, arguments);
         }
     };
-   
+
+    for (var key in data){
+        if ( key!='initialize' && data.hasOwnProperty(key) && (typeof data[key] === 'function') ){
+            c.prototype[key] = data[key];    
+        } 
+    }
 
     return c;
 };
