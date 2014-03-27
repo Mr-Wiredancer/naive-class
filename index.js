40c1ba3af819755cb57dbfa7a19f65d93f8e0786
Mr. Wiredancer <jiahao@dxhackers.com>
2014-03-27 10:25:29 +0800

    small fix

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+var isInternalFunction = function( propName, obj ){
+    return obj.hasOwnProperty(propName);    
+}
+
 module.exports = function( data ){
     var c = function(){
-        if ( data && data.hasOwnProperty('initialize') && (typeof data.initialize === 'function') ){
-            data.initialize.apply(this, arguments);
+        if ( data && isInternalFunction('initialize', data) ){
+            data['initialize'].apply(this, arguments);
         }
     };
 
     for (var key in data){
-        if ( key!='initialize' && data.hasOwnProperty(key) && (typeof data[key] === 'function') ){
+        if ( key!='initialize' && isInternalFunction(key, data) ){
             c.prototype[key] = data[key];    
         } 
     }
